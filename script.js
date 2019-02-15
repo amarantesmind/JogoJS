13695f933485825b0547be2b4a1597c2856c075b
Giovanna <giovannavd@gmail.com>
2019-02-15 09:33:58 -0200

    Identificar os erros

--- script.js
+++ script.js
@@ -2,11 +2,10 @@
 // GERANDO NÚMERO ALEATÓRIO DE O A 50
 
 let aleatorio = function (min, max) {
-    min = Math.ceil(0);
-    max = Math.floor(50);
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
+  let resposta = aleatorio (0, 50);
 // PEDIRNDO PARA O USUÁRIO TENTAR ADIVINHAR O NÚMERO
 
 let numero = (Number(prompt("Tente adivinhar o número! Você têm 10 tentativas")));
@@ -19,13 +18,14 @@ let i = 0;
 while(i <= 10) {
 
 // SE O NÚMERO ESCOLHIDO PELO USUÁRIO (variável numero) FOR IGUAL AO VALOR DA FUNÇÃO (numero gerado aleatoriamente) NOTIFICAR QUE ELE GANHOU
-    if (numero = aleatorio){
+    if (numero == resposta){
         alert("Parabéns, você acertou!");
 }
 // SE O NÚMERO ESCOLHIDO FOR MENOS QUE O VALOR DA FUNÇÃO, AVISAR DO ERRO E PEDIR PARA ESCOLHER DE NOVO
-    else if (numero < aleatorio) {
+    // else if (numero < aleatorio) {mudar condição
         alert("Você errou! O número escolhido é menor que a resposta certa, tente novamente! Menos 1 vida. ");
 
+        // reatribuir o valor da variavel numero para o prompt
         (Number(prompt("Tente adivinhar o número!")));
 
 }
